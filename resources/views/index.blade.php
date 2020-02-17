97359770b634b14d2bfa953e54550bb2ec49502e
Ben Calef <bencalef@gmail.com>
2020-02-17 00:34:56 -0500

    got conditionals working for priorities and regulars, just need to do second column for 'finished'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,21 +11,63 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
 <?php if(count($data['tasks']) < 1): ?>
-    <h4>No tasks yet. Add one to get started.</h4>
+  <h4>No tasks yet. Add one to get started.</h4>
+<?php else: ?>
+  <p>This should print out if there are tasks</p>
+  <!-- Condition for if there are both priority task and regular ones -->
+  <?php if(count($data['priorities']) > 0 && count($data['regulars']) > 0): ?>
+    <p>This means there are priorities and regulars</p>
+    <h4>Priorities</h4>
+    <ul>
+      @foreach($data['priorities'] as $priority)
+        <li>
+          {{ $priority->name }}
+          <a href="/tasks/delete/{{ $priority->id }}"><i class="fas fa-trash-alt"></i></a>
+          <a href="/tasks/prioritize/{{ $priority->id }}"><i class="fas fa-chess-king"></i></a>
+          <a href="/tasks/finish/{{ $priority->id }}"><i class="fas fa-check-square"></i></a>
+        </li>
+      @endforeach
+    </ul>
+    <h4>Regulars</h4>
+    <ul>
+      @foreach($data['regulars'] as $regular)
+        <li>
+          {{ $regular->name }}
+          <a href="/tasks/delete/{{ $regular->id }}"><i class="fas fa-trash-alt"></i></a>
+          <a href="/tasks/prioritize/{{ $regular->id }}"><i class="fas fa-chess-king"></i></a>
+          <a href="/tasks/finish/{{ $regular->id }}"><i class="fas fa-check-square"></i></a>
+        </li>
+      @endforeach
+    </ul>
+  <!-- Condition if there are only priorities -->
+  <?php elseif(count($data['priorities']) > 0 && count($data['regulars']) < 1): ?>
+    <h4>Priorities</h4>
+    <ul>
+      @foreach($data['priorities'] as $priority)
+        <li>
+          {{ $priority->name }}
+          <a href="/tasks/delete/{{ $priority->id }}"><i class="fas fa-trash-alt"></i></a>
+          <a href="/tasks/prioritize/{{ $priority->id }}"><i class="fas fa-chess-king"></i></a>
+          <a href="/tasks/finish/{{ $priority->id }}"><i class="fas fa-check-square"></i></a>
+        </li>
+      @endforeach
+    </ul>
+  <!-- Condition if there are only regular tasks -->
+  <?php elseif(count($data['priorities']) < 1 && count($data['regulars']) > 0): ?>
+    <h4>To Do</h4>
+    <ul>
+      @foreach($data['regulars'] as $regular)
+        <li>
+          {{ $regular->name }}
+          <a href="/tasks/delete/{{ $regular->id }}"><i class="fas fa-trash-alt"></i></a>
+          <a href="/tasks/prioritize/{{ $regular->id }}"><i class="fas fa-chess-king"></i></a>
+          <a href="/tasks/finish/{{ $regular->id }}"><i class="fas fa-check-square"></i></a>
+        </li>
+      @endforeach
+    </ul>
+  <?php endif ?>
 <?php endif ?>
-<?php echo count($data['tasks']) ?>
-<h5>Regular Tasks</h5>
-<ul>
-  @foreach($data['regulars'] as $regular)
-    <li>
-      {{ $regular->name }} - {{ $regular->priority }}
-      <a href="/tasks/delete/{{ $regular->id }}"><i class="fas fa-trash-alt"></i></a>
-      <a href="/tasks/prioritize/{{ $regular->id }}"><i class="fas fa-chess-king"></i></a>
-      <a href="/tasks/finish/{{ $regular->id }}"><i class="fas fa-check-square"></i></a>
-    </li>
-  @endforeach
-  <br>
-</ul>
 
 @endsection
